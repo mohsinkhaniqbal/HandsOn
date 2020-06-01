03b1ba121cf8534fac0b7ed03aa44711d3f46ee0
Mohsin <mohsinkhaniqbal@gmail.com>
2020-06-01 21:21:08 +0530

    x file

---
+++ x.java
@@ -0,0 +1,57 @@
+imort java.util.*;
+
+class Test{
+
+public static void main(String[] args)
+{
+
+
+}
+
+}
+
+class aap{
+
+int age(){
+Scanner sc = new Scanner(System.in)
+int a = sc.nextInt();
+return a;
+
+}
+
+}
+
+class rto{
+
+void xyz(){
+
+if(a<18){
+ua u = new ua();
+
+}
+else if (a>60){
+
+}
+else {
+
+}
+}
+}
+
+class oa extends Exception{
+public String getMessage(){
+return "Old";
+}
+}
+
+class ua extends Exception{
+public String getMessage(){
+return "boy";
+}
+}
+
+
+
+
+
+

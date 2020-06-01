8ef4e86907869a3c526293b335e2ac54beefd92a
Mohsin <mohsinkhaniqbal@gmail.com>
2020-06-01 21:09:32 +0530

    first commit

---
+++ Even.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class Even
+{
+public static void main(String[] args)
+{
+ArrayList c1=new ArrayList();
+c1.add(1.2);
+c1.add(2);
+
+System.out.print(c1);
+}
+
+}
